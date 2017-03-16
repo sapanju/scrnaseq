3f9a687b643de98051354e00b836ea876887187e
sapanju <sapanju313@gmail.com>
2017-03-16 15:30:57 -0400

    Figure labelling, only building predictors for drugs that have balanced class distribution

--- buildpredictor.py
+++ buildpredictor.py
@@ -43,48 +43,58 @@ def main():
         all_inputs = all_inputs[:, ~np.isnan(all_labels)]
         all_labels = all_labels[~np.isnan(all_labels)]
 
-        # Predicted AUC for each cell line (all for the same drug)
-        predicted = np.empty(all_inputs[0].shape)
-        mean_sq_err = np.empty(all_inputs[0].shape)
+        # Only work with drugs that have balanced class distribution (AUC < 0.2 is considered sensitive)
+        num_sensitive = sum(all_labels <= 0.2)
+        num_insensitive = sum(all_labels > 0.2)
 
-        # Leave one out cross validation
-        for i in range(len(all_inputs[0])):
-            fold_identity = np.array(np.zeros(len(all_inputs[0])), dtype=bool)
-            fold_identity[i] = True
+        ratio = (num_insensitive + 0.0)/num_sensitive
 
-            train_inputs = all_inputs[:, ~fold_identity].T
-            train_labels = all_labels[~fold_identity]
-            valid_inputs = all_inputs[:, fold_identity].T
-            valid_labels = all_labels[fold_identity]
+        if (ratio > 0.2 and ratio < 1.8):
+            # Predicted AUC for each cell line (all for the same drug)
+            predicted = np.empty(all_inputs[0].shape)
+            mean_sq_err = np.empty(all_inputs[0].shape)
 
-            # Create linear regression object
-            regr = linear_model.LinearRegression()
+            # Leave one out cross validation
+            for i in range(len(all_inputs[0])):
+                fold_identity = np.array(np.zeros(len(all_inputs[0])), dtype=bool)
+                fold_identity[i] = True
 
-            regr.fit(train_inputs, train_labels)
+                train_inputs = all_inputs[:, ~fold_identity].T
+                train_labels = all_labels[~fold_identity]
+                valid_inputs = all_inputs[:, fold_identity].T
+                valid_labels = all_labels[fold_identity]
 
-            #print('Coefficients: \n', regr.coef_)
-            # The mean squared error
-            print("Drug %d: %s" % (drug, drug_names[drug]))
-            print("Mean squared error: %.2f"
-                  % np.mean((regr.predict(valid_inputs) - valid_labels) ** 2))
-            # Explained variance score: 1 is perfect prediction
-            print('Variance score: %.2f' % regr.score(valid_inputs, valid_labels))
+                # Create linear regression object
+                regr = linear_model.LinearRegression()
 
-            predicted[i] = regr.predict(valid_inputs)
+                regr.fit(train_inputs, train_labels)
 
-            mean_sq_err[i] = (np.mean(predicted[i] - valid_labels) ** 2)
+                #print('Coefficients: \n', regr.coef_)
+                # The mean squared error
+                print("Drug %d: %s" % (drug, drug_names[drug]))
+                print("Mean squared error: %.2f"
+                      % np.mean((regr.predict(valid_inputs) - valid_labels) ** 2))
+                # Explained variance score: 1 is perfect prediction
+                print('Variance score: %.2f' % regr.score(valid_inputs, valid_labels))
+
+                predicted[i] = regr.predict(valid_inputs)
+
+                mean_sq_err[i] = (np.mean(predicted[i] - valid_labels) ** 2)
 
 
-        # Plot outputs
-        plt.scatter(all_labels, predicted, color='black')
-        plt.savefig('drug%d.png' % (drug + 1))
-        plt.close()
-        #plt.plot(valid_inputs, regr.predict(valid_inputs), color='blue', linewidth=3)
+            # Plot outputs
+            plt.scatter(all_labels, predicted, color='black')
+            plt.title("%s" % drug_names[drug])
+            plt.xlabel("Ground truth AUC")
+            plt.ylabel("Predicted AUC")
+            plt.savefig('drug%d.png' % (drug + 1))
+            plt.close()
+            #plt.plot(valid_inputs, regr.predict(valid_inputs), color='blue', linewidth=3)
 
-        #plt.xticks(())
-        #plt.yticks(())
+            #plt.xticks(())
+            #plt.yticks(())
 
-        #plt.show()
+            #plt.show()
 
 
 if __name__ == '__main__':
