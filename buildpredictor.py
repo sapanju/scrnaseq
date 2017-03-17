8453d837779b38ec2748359143db480d5019ec03
sapanju <sapanju313@gmail.com>
2017-03-17 15:43:01 -0400

    Moving load_data functionality to a function

--- buildpredictor.py
+++ buildpredictor.py
@@ -1,15 +1,11 @@
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.neighbors import KNeighborsRegressor
 #import sklearn.cross_validation
 import matplotlib.pyplot as plt
 
 
-def main():
-    # Define parameters
-    K = 7
-    iters = 200
-
-    # Load data
+def load_data():
     auc = np.genfromtxt('GDSC1000_breast_auc.txt', delimiter=',')
     expr = np.genfromtxt('GDSC1000expression_breast.txt', delimiter=',')
 
@@ -27,6 +23,14 @@ def main():
         drug_names = myfile.readlines()
     for line in range(len(drug_names)):
         drug_names[line] = drug_names[line].replace('\n', '').replace('"', '')
+
+    return auc, expr, cell_names, feature_names, drug_names
+
+
+def main():
+
+    # Load data
+    (auc, expr, cell_names, feature_names, drug_names) = load_data()
 
     # Remove columns which are just NA (last 2 cell lines)
     expr = np.delete(expr, 51, axis=1)
@@ -54,7 +58,10 @@ def main():
             predicted = np.empty(all_inputs[0].shape)
             mean_sq_err = np.empty(all_inputs[0].shape)
 
-            # Leave one out cross validation
+            k = cross_validate_select_k(all_inputs, all_labels, drug_names, drug, False)
+            print "Drug %s: k = %d" % (drug_names[drug], k)
+
+            #Leave one out cross validation
             for i in range(len(all_inputs[0])):
                 fold_identity = np.array(np.zeros(len(all_inputs[0])), dtype=bool)
                 fold_identity[i] = True
@@ -65,36 +72,74 @@ def main():
                 valid_labels = all_labels[fold_identity]
 
                 # Create linear regression object
-                regr = linear_model.LinearRegression()
+                #model = linear_model.Ridge(alpha=0.1)
+                model = KNeighborsRegressor(n_neighbors=k, weights='distance')
+                model.fit(train_inputs, train_labels)
 
-                regr.fit(train_inputs, train_labels)
+                #print('Coefficients: \n', model.coef_)
 
-                #print('Coefficients: \n', regr.coef_)
-                # The mean squared error
-                print("Drug %d: %s" % (drug, drug_names[drug]))
-                print("Mean squared error: %.2f"
-                      % np.mean((regr.predict(valid_inputs) - valid_labels) ** 2))
-                # Explained variance score: 1 is perfect prediction
-                print('Variance score: %.2f' % regr.score(valid_inputs, valid_labels))
-
-                predicted[i] = regr.predict(valid_inputs)
+                predicted[i] = model.predict(valid_inputs)
 
                 mean_sq_err[i] = (np.mean(predicted[i] - valid_labels) ** 2)
 
 
             # Plot outputs
-            plt.scatter(all_labels, predicted, color='black')
-            plt.title("%s" % drug_names[drug])
-            plt.xlabel("Ground truth AUC")
-            plt.ylabel("Predicted AUC")
-            plt.savefig('drug%d.png' % (drug + 1))
-            plt.close()
-            #plt.plot(valid_inputs, regr.predict(valid_inputs), color='blue', linewidth=3)
+            fit = np.polyfit(all_labels, predicted, deg=1)
+            if (fit[0] > 0.2 and fit[0] < 1.8):
+                plt.scatter(all_labels, predicted, color='black')
+                plt.plot(all_labels, fit[0] * all_labels + fit[1], color='grey')
+                plt.title("%s" % drug_names[drug])
+                plt.xlabel("Ground truth AUC")
+                plt.ylabel("Predicted AUC")
+                plt.savefig('drug%d.png' % (drug + 1))
+                plt.close()
+                #plt.plot(valid_inputs, model.predict(valid_inputs), color='blue', linewidth=3)
 
-            #plt.xticks(())
-            #plt.yticks(())
+                #plt.xticks(())
+                #plt.yticks(())
 
-            #plt.show()
+                #plt.show()
+
+
+def cross_validate_select_k(all_inputs, all_labels, drug_names, drug, doPlot):
+    num_k_to_test = 10
+    sum_mean_sq_err = np.empty(num_k_to_test)
+
+    for k in range(0, num_k_to_test):
+
+        predicted = np.empty(all_inputs[0].shape)
+        mean_sq_err = np.empty(all_inputs[0].shape)
+
+        # Leave one out cross validation
+        for i in range(len(all_inputs[0])):
+            fold_identity = np.array(np.zeros(len(all_inputs[0])), dtype=bool)
+            fold_identity[i] = True
+
+            train_inputs = all_inputs[:, ~fold_identity].T
+            train_labels = all_labels[~fold_identity]
+            valid_inputs = all_inputs[:, fold_identity].T
+            valid_labels = all_labels[fold_identity]
+
+            model = KNeighborsRegressor(n_neighbors=(k+1), weights='distance')
+            model.fit(train_inputs, train_labels)
+
+            predicted[i] = model.predict(valid_inputs)
+
+            mean_sq_err[i] = (np.mean(predicted[i] - valid_labels) ** 2)
+
+        sum_mean_sq_err[k] = sum(mean_sq_err)
+
+    if (doPlot):
+        plt.scatter(range(1, num_k_to_test+1), sum_mean_sq_err)
+        plt.title("%s Hyperparameter selection" % drug_names[drug])
+        plt.xlabel("K (# of neighbours)")
+        plt.ylabel("Sum of Mean Squared Error")
+        plt.savefig('drug%d_hyperparam_k.png' % (drug + 1))
+        plt.close()
+
+    k_min = np.argmin(sum_mean_sq_err) + 1
+
+    return k_min
 
 
 if __name__ == '__main__':
